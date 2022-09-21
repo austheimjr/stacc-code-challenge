ee911fe6971090adbfff590658a030afa906095b
austheimjr <austheim.marius@gmail.com>
2022-09-21 16:29:56 +0200

    lets user search for name, returns number of hits

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,10 +1,38 @@
+import React, { useState } from "react";
+
+
 export default function Search() {
+
+   const [search, setSearch] = useState("");
+   const [person, setPerson] = useState([]);
+
+  const getPerson = async () => {
+    try {
+      const response = await fetch(`https://code-challenge.stacc.dev/api/pep?name=${search}`);
+      const json = await response.json();
+      setPerson(json);
+      console.log(json);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  
+  
+
+    
+
     return (
       <div className="search">
         <h1>Search</h1>
-        <p>
-          This is the search page. It's a great place to start.
-        </p>
-      </div>
+        <input
+          type="text"
+          placeholder="Search for a person"
+          onChange={(event) => setSearch(event.target.value)}
+        />
+        <button onClick={getPerson}>Search</button>
+        <h1>{person.numberOfHits} </h1>
+    
+    </div>
     );
   }
