347301456d567422021f54e802f1d2f64c274938
austheimjr <austheim.marius@gmail.com>
2022-09-22 18:52:52 +0200

    styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,11 +8,11 @@ export default function Home() {
         </p>
         <div>
           <h3>What does PEP mean?</h3>
-          <br></br>
+          
           <p>A politically exposed person (PEP) is a person who is or has been entrusted with a prominent public function.</p>
-          <br></br>
+          
           <h3>Why do we need to know if a person is a PEP?</h3>
-          <br></br>
+          
           <p>A PEP is a person who is or has been entrusted with a prominent public function. This means that they are in a position of trust and have access to sensitive information. This makes them a potential target for bribery and corruption.</p>
           
         </div>
